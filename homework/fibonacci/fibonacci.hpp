5f4d6284520f7b2caabbc9550b26d611f06224fd
szymon <szy.gladysz@proton.me>
2026-09-16 22:22:56 +0200

    implemented fibonacci

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,37 @@
 #pragma once
 
+// https://pl.wikipedia.org/wiki/Ci%C4%85g_Fibonacciego
+
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {
+        return 0;
+    }
+    if (sequence == 1) {
+        return 1;
+    }
+
+    int f0 = 0;
+    int f1 = 1; 
+    
+    int current = 1;
+    
+    for (std::size_t i = 2; i < sequence + 1; ++i)
+    {
+        current = f1 + f0;   
+        f0 = f1;
+        f1 = current;
+    }
+    return f1; // Fn = F(n-1) + F (n-2)
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 0) {
+        return 0;
+    }
+    if (sequence == 1) {
+        return 1;
+    }
+    else {
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2); 
+    }
 }
