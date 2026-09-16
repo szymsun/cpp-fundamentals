919e46e3d3c2a8db4c4f712dbaa272eb9e5f6634
szymon <szy.gladysz@proton.me>
2026-09-16 23:08:09 +0200

    implemented gcd and lcm

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    // euclidean implementation
+    int i = 2;
+    while (rhs != 0) {
+        if (lhs > rhs) {
+            lhs %= rhs;
+            std::swap(lhs, rhs);
+        } else {
+            rhs %= lhs;
+            std::swap(lhs, rhs);
+        }
+    }
+    return lhs;
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(int lhs, int rhs) {  // kinda lazy but works
+    return lhs * rhs / (NWD(lhs, rhs));
 }
