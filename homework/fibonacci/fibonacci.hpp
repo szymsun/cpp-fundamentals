3e8863ac89af048b219d409e6b364a81d051542e
szymon <szy.gladysz@proton.me>
2026-09-16 22:27:43 +0200

    clang formaat

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -11,17 +11,16 @@ int fibonacci_iterative(int sequence) {
     }
 
     int f0 = 0;
-    int f1 = 1; 
-    
+    int f1 = 1;
+
     int current = 1;
-    
-    for (std::size_t i = 2; i < sequence + 1; ++i)
-    {
-        current = f1 + f0;   
+
+    for (std::size_t i = 2; i < sequence + 1; ++i) {
+        current = f1 + f0;
         f0 = f1;
         f1 = current;
     }
-    return f1; // Fn = F(n-1) + F (n-2)
+    return f1;  // Fn = F(n-1) + F (n-2)
 }
 
 int fibonacci_recursive(int sequence) {
@@ -30,8 +29,7 @@ int fibonacci_recursive(int sequence) {
     }
     if (sequence == 1) {
         return 1;
-    }
-    else {
-        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2); 
+    } else {
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
     }
 }
