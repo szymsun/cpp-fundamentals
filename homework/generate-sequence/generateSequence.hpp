5b83e5eed645c35d2ddcb1883e7e7117b9956229
szymon <szy.gladysz@proton.me>
2026-09-20 15:42:26 +0200

    implemented generateSequence

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,16 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> out = {};
+
+    if (count <= 0){
+        return {};
+    }
+
+    for (size_t i = 0; i < count; i++)
+    {
+        out.push_back(step * (i+1));
+    }
+
+    return out;
 }
